344bfaf3a27a3e4bbe02ff19d56eaaaf104ff4bb
Mariane Fontes <116356964+marianefontes@users.noreply.github.com>
2024-09-29 18:07:41 -0300

    fix: padronizando consumidores

--- consumidor/src/main/consumidor6.py
+++ consumidor/src/main/consumidor6.py
@@ -12,7 +12,31 @@ channel.exchange_declare(exchange='channel_exchange', exchange_type='topic')
 result = channel.queue_declare('', exclusive=True)
 queue_name = result.method.queue
 
-routing_key = "rota.um.#"
+# As rotas disponíveis pra escolha:
+rotas = {
+    '1': 'Papo com CHOQUEI (Gustavo Lima, Deolane, Juliette)', # Rota pra Choquei que tem Gustavo Lima, Deolane, Juliette
+    '2': 'Papo com astros da Música',                          # Rota pra Música que tem Gustavo Lima, Deolane, Juliette
+    '3': 'Papo com Gustavo Lima',                              # Rota individual pra Gustavo Lima
+    '4': 'Papo com Deolane',                                   # Rota individual pra Deolane
+    '5': 'Papo com Juliette',                                  # Rota individual pra Juliette
+    '6': 'Papo exclusivo com CHOQUEI e Gustavo Lima',          # Rota pra Choquei e Gustavo Lima
+}
+
+# A pessoa vai ver as rotas disponíveis naquele dia:
+print("Veja as rotas dispoíveis para participar:")
+for key, route in rotas.items():
+    print(f"{key}: {route}")
+
+# Agora ela vai escolher:
+while True:
+    escolha = input("Agora escolha com que você vai interagir hoje:")
+
+    # Validação:
+    if escolha in rotas:
+        routing_key = rotas[escolha]
+        break
+    else:
+        print("Escolha inválida. Tente novamente.")
 
 # Binding da fila com o exchange e a rota especificada
 channel.queue_bind(exchange='channel_exchange', queue=queue_name, routing_key=routing_key)
