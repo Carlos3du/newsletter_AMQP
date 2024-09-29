b52ad3a748e3d1743fe417d36e44acf3e7d44be9
Gislaine Reis <gmvbr.2002@gmail.com>
2024-09-29 11:26:21 -0300

    padronizando as mensagens

--- prodmesseger/src/main/java/com/example/prodmessenger/Runner.java
+++ prodmesseger/src/main/java/com/example/prodmessenger/Runner.java
@@ -1,5 +1,7 @@
 package com.example.prodmessenger;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
@@ -21,21 +23,36 @@ public class Runner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
+        // Perguntar o nome do influenciador uma vez no início
+        System.out.print("Digite o nome do influenciador: ");
+        String nomeDoInfluenciador = scanner.nextLine();
+
         while (true) {
             System.out.println("Digite a mensagem ou 'sair' para encerrar:");
-            String msg = scanner.nextLine();
+            String corpoDaMensagem = scanner.nextLine();
 
-            if (msg.equalsIgnoreCase("sair")) {
+            if (corpoDaMensagem.equalsIgnoreCase("sair")) {
                 break;
             }
 
-            // Envia a mensagem para o exchange e a rota especificada
-            rabbitTemplate.convertAndSend(ProdmessegerApplication.topicExchangeName, ProdmessegerApplication.routingKey, msg);
+            // Formatar a mensagem com a data e hora atuais, o nome do influenciador e o corpo da mensagem
+            String mensagemFormatada = formatarMensagem(nomeDoInfluenciador, corpoDaMensagem);
 
-            System.out.println("Mensagem enviada: " + msg);
+            // Envia a mensagem para o exchange e a rota especificada
+            rabbitTemplate.convertAndSend(ProdmessegerApplication.topicExchangeName, ProdmessegerApplication.routingKey, mensagemFormatada);
+
+            System.out.println("Mensagem enviada: " + mensagemFormatada);
         }
 
         // Fecha o contexto do Spring quando o loop terminar
         context.close();
+    }
+
+    // Metodo para formatar a mensagem no padrão desejado
+    private String formatarMensagem(String nomeDoInfluenciador, String corpoDaMensagem) {
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm");
+        String dataHora = agora.format(formatter);
+        return String.format("[%s] %s : %s", dataHora, nomeDoInfluenciador, corpoDaMensagem);
     }
 }
